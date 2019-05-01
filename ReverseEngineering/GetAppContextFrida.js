a407f4fb39f0511397c82736809f2569fc1008e6
Dimi Vals <valsamaras@gmail.com>
2019-05-01 15:18:52 +0300

    Create GetAppContextFrida.js

---
+++ ReverseEngineering/GetAppContextFrida.js
@@ -0,0 +1,12 @@
+
+ 
+Java.perform(function () {
+
+
+	var currentApplication = Java.use('android.app.ActivityThread').currentApplication();
+
+	var context = currentApplication.getApplicationContext();
+
+    
+})
+
