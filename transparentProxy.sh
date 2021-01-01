03f2f6042692441a6012d8ceaabf7f2827a542c9
Dimitris Valsamaras <valsamaras@gmail.com>
2021-01-01 10:46:20 +0000

    transproxy fix

--- transparentProxy.sh
+++ transparentProxy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+
+
 iptables -t nat -F
-iptables -t nat -A OUTPUT -p tcp --dport 80 -j DNAT --to-destination <proxyip>:port
-iptables -t nat -A OUTPUT -p tcp --dport 443 -j DNAT --to-destination <proxyip>:port
+iptables -t nat -A OUTPUT -p tcp --dport 80 -j DNAT --to-destination $1:$2
+iptables -t nat -A OUTPUT -p tcp --dport 443 -j DNAT --to-destination $1:$2
 iptables -t nat -A POSTROUTING -p tcp --dport 80 -j MASQUERADE
 iptables -t nat -A POSTROUTING -p tcp --dport 443 -j MASQUERADE
 
